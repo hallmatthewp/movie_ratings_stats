782e1c6eea5219cbb089aff219c92fe1de014628
Micah Ransdell <mransdell@netflix.com>
2014-06-05 23:28:59 -0700

    Minor cleanup

--- endpoint/Ratings.groovy
+++ endpoint/Ratings.groovy
@@ -1,19 +1,12 @@
 import rx.Observable;
-import com.netflix.api.platform.JsonUtility
-import com.netflix.api.service.APIWatchableExtensions
 import com.netflix.api.service.APIRequest
 import com.netflix.api.endpoint.APIEndpoint
 
-import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import java.util.concurrent.TimeUnit;
 
 public class Ratings extends APIEndpoint {
-
     @Override
     public void execute(APIRequest api) {
-        def makeItJSON = { JsonUtility.jsonFromMap(it); }
-        HttpServletRequest request = api.getServletRequest()
         HttpServletResponse response = api.getServletResponse()
 
         response.setContentType("application/json; charset=UTF-8")
